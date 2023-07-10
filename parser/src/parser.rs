f50d06a33f3c8221a4e4f918bc8c3e237baf04e3
Zanie <contact@zanie.dev>
2023-07-10 12:46:17 -0500

    Add test

--- parser/src/parser.rs
+++ parser/src/parser.rs
@@ -1099,5 +1099,23 @@ def args_to_tuple(*args: *Ts) -> Tuple[*Ts]: ...
     fn test_parse_identifier() {
         let i = ast::Identifier::parse_without_path("test").unwrap();
         assert_eq!(i.as_str(), "test");
+        
+    #[cfg(not(feature = "all-nodes-with-ranges"))]
+    fn decorator_ranges() {
+        let parse_ast = parse_program(
+            r#"
+@my_decorator
+def test():
+    pass
+    
+@class_decorator
+class Abcd:
+    pass
+"#
+            .trim(),
+            "<test>",
+        )
+        .unwrap();
+        insta::assert_debug_snapshot!(parse_ast);
     }
 }
